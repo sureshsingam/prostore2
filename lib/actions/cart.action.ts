8f103596886d598b29bcf2e4cca4bd06705db87a
Suresh <sureshsingam@gmail.com>
2025-07-19 01:12:26 -0400

    Enhances cart functionality with validation and database integration
    Replaces placeholder implementation in `addItemToCart` with a robust
    process for session validation, user authentication, and product
    lookup. Introduces `getMyCart` to retrieve cart details, ensuring
    correct handling of session and user-specific data.
    
    Updates default sign-in values for testing purposes.
    
    Improves error handling and data formatting for better usability.

--- lib/actions/cart.action.ts
+++ lib/actions/cart.action.ts
@@ -1,9 +1,74 @@
 "use server";
 import { CartItem } from "@/types";
+import { cookies } from "next/headers";
+import { convertToPlainObject, formatErrors } from "../utils";
+import { auth } from "@/auth";
+import { prisma } from "@/db/prisma";
+import { cartItemSchema } from "../validators";
 
 export async function addItemToCart(data: CartItem) {
-  return {
-    success: true,
-    message: "Item added to cart",
-  };
+  try {
+    //check for cart Cookie
+    const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+    if (!sessionCartId) throw new Error("Cart Session not found");
+
+    // Get session and user ID
+    const session = await auth();
+    const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+    //Get Cart
+    const cart = await getMyCart();
+
+    // Parse and validate item
+    const item = cartItemSchema.parse(data);
+    // Find product in database
+    const product = await prisma.product.findFirst({
+      where: { id: item.productId },
+    });
+
+    //testing
+    console.log({
+      "session cart id": sessionCartId,
+      "User Id": userId,
+      "Item Requested": item,
+      "Product Found": product,
+    });
+
+    return {
+      success: true,
+      message: "Item added to cart",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: formatErrors(error),
+    };
+  }
+}
+
+export async function getMyCart() {
+  //check for cart Cookie
+  const sessionCartId = (await cookies()).get("sessionCartId")?.value;
+  if (!sessionCartId) throw new Error("Cart Session not found");
+
+  // Get session and user ID
+  const session = await auth();
+  const userId = session?.user?.id ? (session.user.id as string) : undefined;
+
+  // Get user Cart from database
+  const cart = await prisma.cart.findFirst({
+    where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+  });
+
+  if (!cart) return undefined;
+
+  // convert decimals and return
+  return convertToPlainObject({
+    ...cart,
+    itesm: cart.items as CartItem[],
+    itemsPrice: cart.itemsPrice.toString(),
+    totalPrice: cart.totalPrice.toString(),
+    shippingPrice: cart.shippingPrice.toString(),
+    taxPrice: cart.taxPrice.toString(),
+  });
 }
