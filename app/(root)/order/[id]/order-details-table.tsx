65bd145d7132e2732b1a744761da5dc0f6f93f49
Suresh <sureshsingam@gmail.com>
2025-08-03 16:50:55 -0400

    Prepared  Order Details Page

--- app/(root)/order/[id]/order-details-table.tsx
+++ app/(root)/order/[id]/order-details-table.tsx
@@ -1,6 +1,18 @@
 "use client";
-import { formatId } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { formatCurrency, formatDateTime, formatId } from "@/lib/utils";
 import { Order } from "@/types";
+import Link from "next/link";
+import Image from "next/image";
 
 const OrderDetailsTable = ({ order }: { order: Order }) => {
   const {
@@ -8,18 +20,112 @@ const OrderDetailsTable = ({ order }: { order: Order }) => {
     shippingAddress,
     orderitems,
     itemsPrice,
+    shippingPrice,
     taxPrice,
     totalPrice,
     paymentMethod,
     isDelivered,
     isPaid,
+    paidAt,
+    deliveredAt,
   } = order;
 
   return (
     <>
       <h1 className="py-4 text-2xl">Order {formatId(id)}</h1>
       <div className="grid md:grid-cols-3 md:gap-5">
-        <div className="col-span-2 space-4-y overflow-x-auto">Content</div>
+        <div className="col-span-2 space-4-y overflow-x-auto">
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <h2 className="text-xl pb-4"> Payment Method</h2>
+              <p className="mb-2"> {paymentMethod}</p>
+              {isPaid ? (
+                <Badge variant="secondary">
+                  Paid at {formatDateTime(paidAt!).dateTime}
+                </Badge>
+              ) : (
+                <Badge variant="destructive"> Not Paid</Badge>
+              )}
+            </CardContent>
+          </Card>
+          <Card className="my-2">
+            <CardContent className="p-4 gap-4">
+              <h2 className="text-xl pb-4"> Shipping Address</h2>
+              <p> {shippingAddress.fullName}</p>
+              <p className="mb-2">
+                {shippingAddress.streetAddress},{shippingAddress.city}
+                {shippingAddress.postalCode},{shippingAddress.country}
+              </p>
+              {isDelivered ? (
+                <Badge variant="secondary">
+                  Delivered at {formatDateTime(deliveredAt!).dateTime}
+                </Badge>
+              ) : (
+                <Badge variant="destructive"> Not Delivered</Badge>
+              )}
+            </CardContent>
+          </Card>
+          <Card>
+            <CardContent className="p-4 gap-4">
+              <h2 className="text-xl pb-4"> Order Items </h2>
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Item</TableHead>
+                    <TableHead>Quantity</TableHead>
+                    <TableHead>Price</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {orderitems.map((item) => (
+                    <TableRow key={item.slug}>
+                      <TableCell>
+                        <Link
+                          href={`/product/${item.slug}`}
+                          className="flex items-center"
+                        >
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          />
+                          <span className="px-2"> {item.name}</span>
+                        </Link>
+                      </TableCell>
+                      <TableCell>
+                        <span className="px-2">{item.quantity}</span>
+                      </TableCell>
+                      <TableCell>${item.price}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </CardContent>
+          </Card>
+        </div>
+        <div>
+          <Card>
+            <CardContent className="p-4 gap-4 space-y-4">
+              <div className="flex justify-between">
+                <p>Items</p>
+                <p>{formatCurrency(itemsPrice)}</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Tax</p>
+                <p>{formatCurrency(taxPrice)}</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Shipping</p>
+                <p>{formatCurrency(shippingPrice)}</p>
+              </div>
+              <div className="flex justify-between">
+                <p>Total</p>
+                <p>{formatCurrency(totalPrice)}</p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </>
   );
