aa8c5dd29731481a93d6ad44bc080d7925e2a5e5
Suresh <sureshsingam@gmail.com>
2025-09-14 18:44:28 -0400

    Added Featured Product Carousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,20 @@
 // import { Button } from "@/components/ui/button";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" />
     </>
   );
