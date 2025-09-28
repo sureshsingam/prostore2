6babf57ef9ae9ce3bd4e2f9f8fa0b7f663060c15
Suresh <sureshsingam@gmail.com>
2025-09-28 01:27:54 -0400

    Added Dynamic Metadata so that page title shows selected search criteria

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -32,6 +32,42 @@ const prices = [
 const ratings = [4, 3, 2, 1];
 const sortOrders = ["newest", "lowest", "highest", "rating"];
 
+export async function generateMetadata(props: {
+  searchParams: Promise<{
+    q: string;
+    category: string;
+    price: string;
+    rating: string;
+  }>;
+}) {
+  const {
+    q = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+  } = await props.searchParams;
+
+  const isQuerySet = q && q !== "all" && q.trim() !== "";
+  const isCategorySet =
+    category && category !== "all" && category.trim() !== "";
+  const isPriceSet = price && price !== "all" && price.trim() !== "";
+  const isRatingSet = rating && rating !== "all" && rating.trim() !== "";
+
+  if (isQuerySet || isCategorySet || isPriceSet || isRatingSet) {
+    return {
+      title: `
+      Search ${isQuerySet ? q : ""}
+       ${isCategorySet ? `: Category ${category}` : ""}
+        ${isPriceSet ? `Price ${price}` : ""} 
+        ${isRatingSet ? `Rating ${rating}` : ""}`,
+    };
+  } else {
+    return {
+      title: "Search Products",
+    };
+  }
+}
+
 const SearchPage = async (props: {
   searchParams: Promise<{
     q?: string;
